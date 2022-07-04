e010a54ceb3e08f1206df0dd14b7d01d64676536
@manuelduarte077 <carlosd99m@gmail.com>
2022-07-03 18:07:13 -0600

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Manuel Duarte on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
