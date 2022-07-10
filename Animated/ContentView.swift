fe5eba0c84b16f6f4c04c2e76a76fcc6a3b74ae9
@manuelduarte077 <carlosd99m@gmail.com>
2022-07-10 02:28:19 -0600

    icon animation

--- Animated/ContentView.swift
+++ Animated/ContentView.swift
@@ -1,7 +1,16 @@
 import SwiftUI
+import RiveRuntime
+
+
 
 struct ContentView: View {
     @AppStorage("selectedTab") var selectedTab: Tab = .chat
+    
+    let button = RiveViewModel(fileName: "menu_button")
+    
+    @State var isOpen = false
+    
+    
     var body: some View {
         ZStack {
             switch selectedTab {
@@ -16,6 +25,17 @@ struct ContentView: View {
                 case .user:
                     Text("User")
             }
+            button.view()
+                .frame(width: 44, height: 44)
+                .mask(Circle())
+                .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x: 0, y: 5)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .padding()
+                .onTapGesture {
+                    try? button.setInput("isOpen", value: isOpen)
+                    isOpen.toggle()
+                }
+            
             
             TabBar()
         }
